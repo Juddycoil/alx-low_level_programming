51f68abd13f68e32f2023c3af3787b9d77712773
Juddycoil <david.o.julius@gmail.com>
2022-07-08 10:01:28 -0700

    task 5 and 6

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -8,9 +8,9 @@
 void print_numbers(void)
 
 {
-int num = 48;
-	for (num = 48; num < 58; num++)
-	_putchar(num);
+int number = 48;
+	for (number = 48; number < 58; number++)
+	_putchar(number);
 _putchar(10);
 
 }
---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_line - print a straight line
+ * @n: parameter
+ *
+ * Return: nothing
+ */
+
+void print_line(int n)
+{
+while (n-- > 0)
+{
+	_putchar('_');
+}
+_putchar(10);
+}
