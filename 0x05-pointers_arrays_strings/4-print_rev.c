aaa2d73d7059852c707235a2ecc933e05c9e9f53
Juddycoil <david.o.julius@gmail.com>
2022-07-13 13:40:16 -0700

    fix

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,18 +8,8 @@
  */
 void print_rev(char *s)
 {
-int b, e, c = 0;
 
-while (s[c] != '\0')
-	c++;
-e = c - 1;
-
-for (b = 0; b < c; b++)
-{
-s[b] = s[e];
-e--;
-}
-s[b] = '\0';
+       for (s = '\0'; s--;)
 {
 	_putchar(*s);
 }
