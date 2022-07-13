3354c71a8e18b18e4cfcede3cbabb05fa88f4b49
Juddycoil <david.o.julius@gmail.com>
2022-07-12 21:21:50 -0700

    point to an int

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * reset_to_98 -  pointer to an int as parameter
+ * @n: pointer
+ * Return: nothing
+ */
+void reset_to_98(int *n);
+{
+int a = 98;
+	n = &a;
+	{
+	_putchar(n);
+	_putchar('\n');
+	}
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,5 @@
+## 0x05. C - Pointers, arrays and strings
+
+#This a project fromn ALXSE to learn more about Pointers, Arrays and strings in C programing .
+
+#Task 0 - 9 and advance task are listed in this directory 
