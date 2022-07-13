5edfcd56b5b591394e01ac20eff0a2b828bec51f
Juddycoil <david.o.julius@gmail.com>
2022-07-13 12:34:31 -0700

    string in reverse

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_rev - print string in reverse
+ * @s: parameter for string
+ *
+ * Return: nothing
+ */
+void print_rev(char *s)
+{
+for (; *s != '\0'; s--)
+{
+	_putchar(*s);
+}
+	_putchar('\n');
+}
