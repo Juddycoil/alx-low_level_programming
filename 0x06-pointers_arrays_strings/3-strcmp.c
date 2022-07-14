18a789da5a460b8085b0258174ab466057c5c747
Juddycoil <david.o.julius@gmail.com>
2022-07-14 13:49:10 -0700

    compare strings

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -5,25 +5,13 @@
 * _strcmp - compares two strings
 * @s1: param 1
 * @s2: param 2
-* Return: 0
+* Return: string value
 */
 int _strcmp(char *s1, char *s2)
 {
 int value;
 
 value = strcmp(s1, s2);
-if (value == 0)
-{
-_putchar('0');
-}
 
-else if (s1 < s2)
-{
-_putchar('-');
-}
-else
-{
-_putchar('+');
-}
-return (0);
+return (value);
 }
