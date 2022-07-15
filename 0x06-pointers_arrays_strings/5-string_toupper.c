dcc4dd5eaae4e4c10866e93f7b297a06bf5c574d
Juddycoil <david.o.julius@gmail.com>
2022-07-14 23:11:40 -0700

    fix upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -3,21 +3,18 @@
 /**
 * *string_toupper - convert lowercase in a string to uppercase
 * @ch: param
-* Return: something
+* Return: string
 */
 char *string_toupper(char *ch)
-
 {
 int a = 0;
 
 while (ch[a])
 {
-
 if (ch[a] >= 97 && ch[a] <= 122)
-{	
 ch[a] = ch[a] - 32;
-	a++;
-}
+a++;
+
 }
 return (ch);
 }
