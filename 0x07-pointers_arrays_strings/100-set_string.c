eda4e155eb01e7696009a46961f2e36256565dd8
Juddycoil <david.o.julius@gmail.com>
2022-07-19 07:51:40 -0700

    return pointer to value

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* set_string - set the value of a pointer to a char
+* @s: pointer param
+* @to: assign param
+* Retrun: pointer value
+*/
+
+void set_string(char **s, char *to)
+{
+
+return (*s = to);
+}
