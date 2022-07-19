9ca8010a71786fdf5b42787bdd8df172f3836575
Juddycoil <david.o.julius@gmail.com>
2022-07-19 07:54:08 -0700

    quick fix

--- 0x07-pointers_arrays_strings/100-set_string.c
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -4,11 +4,11 @@
 * set_string - set the value of a pointer to a char
 * @s: pointer param
 * @to: assign param
-* Retrun: pointer value
+* Retrun: nothing
 */
 
 void set_string(char **s, char *to)
 {
 
-return (*s = to);
+*s = to;
 }
