85c1215af79a77a81afb1763644d06c1f0930633
Juddycoil <david.o.julius@gmail.com>
2022-07-20 03:54:04 -0700

    string length with recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _strlen_recursion - string length in recursion
+* @s: string
+* Return: Length of string
+*/
+
+int _strlen_recursion(char *s)
+{
+
+if (*s == '\0')
+	return (0);
+s++;
+return (1 + _strlen_recursion(s));
+
+}
