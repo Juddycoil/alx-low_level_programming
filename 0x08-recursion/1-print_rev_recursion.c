ea07454f5499a22ce6fbeef0399aaaa257513974
Juddycoil <david.o.julius@gmail.com>
2022-07-20 01:07:23 -0700

    reverse string

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+
+/**
+* _print_rev_recursion - print string in reverse
+* @s: string param
+*/
+void _print_rev_recursion(char *s)
+{
+int a;
+for (a = 0; a <= '\0'; a--)
+
+{
+_putchar(s[a]);
+
+}
+}
