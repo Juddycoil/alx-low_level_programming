a14fab497df9d06462b7ee3e064559b788bc1869
Juddycoil <david.o.julius@gmail.com>
2022-07-20 01:56:16 -0700

    fix

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+#include <string.h>
 
 /**
 * _print_rev_recursion - print string in reverse
@@ -7,12 +7,10 @@
 */
 void _print_rev_recursion(char *s)
 {
-
-while (*s == '\0')
-s--;
+if (*s == '\0')
+	s--;
 {
 _putchar('\n');
 }
-
 _print_rev_recursion(s);
 }
