040b772e4015de6d55c442506eab458000af574c
Juddycoil <david.o.julius@gmail.com>
2022-07-22 09:22:05 -0700

    create a library

--- 0x09-static_libraries/create_static_lib.sh
+++ 0x09-static_libraries/create_static_lib.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-gcc -Wall -pedantic -Werror -Wextra -std=gnu89 -c *.c && ar -rc liball.a *.o
+echo "Compiling..."
+gcc -Wall -pedantic -Wextra -Werror -std=gnu89 -c *.c
+echo "Compile successful!"
+ar -rc liball.a *.o
+echo "Compiled object files"
+echo "====================="
+ar -t liball.a
+echo "Complete."
