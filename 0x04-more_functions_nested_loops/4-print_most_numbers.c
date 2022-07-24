c7c1a871f59edea4247da2f32e2cea4cd956a6dd
Juddycoil <david.o.julius@gmail.com>
2022-07-24 00:31:46 -0700

    fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,11 +8,12 @@ void print_most_numbers(void)
 {
 int x;
 {
-	for (x = 0; x <= 9; x++)
+	for (x = 0; x <= 9;)
 	{
 	while (x != 2 && x != 4)
-		_putchar(x + '0');
+		x++;
 	}
+	_putchar(x + '0');
 }
 _putchar(10);
 
