d5b4dcf6435c851e2ea1c9c68ca7eb212b8299bf
Juddycoil <david.o.julius@gmail.com>
2022-07-24 11:39:29 -0700

    create an array in dynamic memory

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+* *create_array - creates an array of chars, & initializes it with a specific char
+* @size: size of memory
+* @c: array param
+* Return: 0 or pointer to array
+*/
+
+char *create_array(unsigned int size, char c)
+{
+	char *a;
+	unsigned int i;
+
+	a = malloc(size * sizeof(char));
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	i = 0;
+	while (i < size)
+	{
+		a[i] = c;
+		i++;
+	}
+	a[i] = '\0';
+	return (a);
+
+}
---
+++ 0x0B-malloc_free/README.md
@@ -0,0 +1,5 @@
+## 0x0B. C - malloc, free
+
+#This a project from ALX SE to learn about the functions of malloc() and free(), used for dynamic memory allocation and to free up space in memory.
+
+#Task 0 - 6 and advance task are listed in this directory 
