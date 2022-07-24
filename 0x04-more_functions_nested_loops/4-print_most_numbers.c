28aefcd922fcad84f2f20ae371dafbdef460b220
Juddycoil <david.o.julius@gmail.com>
2022-07-24 00:21:33 -0700

    fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,15 +4,15 @@
  * print_most_numbers - print number from zero to nine
  * except two and four
  */
-void print_most_numbers(void);
+void print_most_numbers(void)
 {
 int x;
 {
-for (x = 0; x < 10; x++)
-{
-if (x != 2 && x != 4)
-	_putchar(x + '0');
-}
+	for (x = 0; x <= 9; x++)
+	{
+	while (x != 2 && x != 4)
+		_putchar(x + '0');
+	}
 }
 _putchar(10);
 
