d391b742ecc9622ed7eec8781f4c866bc68c852c
Juddycoil <david.o.julius@gmail.com>
2022-08-22 13:04:41 -0700

    fix

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -2,7 +2,7 @@
 
 /**
  * read_textfile - reads a text file and prints it to the standard output
- * @filename: name of the file to be read
+ * @filename: file to be read
  * @letters: number of letters to read and print
  *
  * Return: the number of letters printed, or 0 if it failed
@@ -11,8 +11,8 @@
 ssize_t read_textfile(const char *filename, size_t letters)
 {
 	int fd;
-	int s, t;
-	char *buf;
+	int r, w;
+	char *buff;
 
 	if (!filename)
 		return (0);
@@ -21,28 +21,28 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	if (fd < 0)
 		return (0);
 
-	buf = malloc(sizeof(char) * letters);
-	if (!buf)
+	buff = malloc(sizeof(char) * letters);
+	if (!buff)
 		return (0);
 
-	s = read(fd, buf, letters);
-	if (s < 0)
+	r = read(fd, buf, letters);
+	if (r < 0)
 	{
-		free(buf);
+		free(buff);
 		return (0);
 	}
-	buf[s] = '\0';
+	buff[r] = '\0';
 
 	close(fd);
 
-	t = write(STDOUT_FILENO, buf, s);
-	if (t < 0)
+	w = write(STDOUT_FILENO, buf, s);
+	if (w < 0)
 	{
-		free(buf);
+		free(buff);
 		return (0);
 	}
 
-	free(buf);
-	return (t);
+	free(buff);
+	return (w);
 }
 
